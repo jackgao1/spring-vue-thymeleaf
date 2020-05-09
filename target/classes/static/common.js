74040b4fc87f843323da492491763f53c04faae6
jackgao1 <50727687+jackgao1@users.noreply.github.com>
2020-05-09 16:14:37 +0800

    删除垃圾文件

--- target/classes/static/common.js
+++
@@ -1,6 +0,0 @@
-document.write('<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>');
-document.write('<script src="https://unpkg.com/element-ui/lib/index.js"></script>');
-document.write('<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">');
-document.write('<script src="https://cdn.jsdelivr.net/vue.resource/1.0.3/vue-resource.min.js"></script>');
-document.write('<script src="https://unpkg.com/axios/dist/axios.min.js"></script>');
-
